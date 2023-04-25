2a7f2830d0762577dded76f9e4d914ba358e88e7
LongLhai <124671185+LongLhai@users.noreply.github.com>
2023-04-25 15:38:55 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,125 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.915110, 8.086632]
+        },
+        "properties": {
+          "name": "ถ้ำพระนางนอน",
+          "type": "Natural",
+          "level": 5
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.835555, 8.052694]
+        },
+        "properties": {
+          "name": "อ่าวพระนาง",
+          "type": "Natural",
+          "level": 8
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.919439, 7.738709]
+        },
+        "properties": {
+          "name": "เกาะปอดะ",
+          "type": "Natural",
+          "level": 7
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.765645, 8.020536]
+        },
+        "properties": {
+          "name": "เกาะเฮ",
+          "type": "Special",
+          "level": 9
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [99.073061, 7.892364]
+        },
+        "properties": {
+          "name": "เกาะลันตา",
+          "type": "Natural",
+          "level": 6
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.759997, 8.046465]
+        },
+        "properties": {
+          "name": "อ่าวน้ำหมาก",
+          "type": "Culture",
+          "level": 4
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [98.621924, 8.277903]
+        },
+        "properties": {
+          "name": "น้ำตกห้วยท้วม",
+          "type": "Natural",
+          "level": 3,
+}
+},
+{
+"type": "Feature",
+"geometry": {
+"type": "Point",
+"coordinates": [98.882688, 8.057680]
+},
+"properties": {
+"name": "เกาะตะปู",
+"type": "Special",
+"level": 2
+}
+},
+{
+"type": "Feature",
+"geometry": {
+"type": "Point",
+"coordinates": [98.631123, 8.193454]
+},
+"properties": {
+"name": "เขานาคเกิด",
+"type": "Culture",
+"level": 10
+}
+},
+{
+"type": "Feature",
+"geometry": {
+"type": "Point",
+"coordinates": [98.730402, 8.041134]
+},
+"properties": {
+"name": "หาดอ่าวนาง",
+"type": "Natural",
+"level": 1
+}
+}
+]
+}
